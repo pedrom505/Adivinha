18e375a0e854ff78d681dd330128371cf9390ca0
Pedro Pereira <pedrom314@gmail.com>
2018-01-31 00:09:36 -0200

    This is the first version of this App In this version was include the follow alterations: - Created the first version of main view - Tested the interaction between main view and the code main.Java

---
+++ app/src/main/java/adivinha/decisionmind/com/adivinha/MainActivity.java
@@ -0,0 +1,23 @@
+package adivinha.decisionmind.com.adivinha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botaoJogar;
+    private TextView textoResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoJogar = findViewById(R.id.botaoJogarId);
+        textoResultado = findViewById(R.id.resultadoId);
+
+        textoResultado.setText("Texto alterado");
+    }
+}
