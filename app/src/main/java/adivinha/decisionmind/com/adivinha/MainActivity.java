595756ed44c0d271829da58ccdd1ebf9bd0d9710
Pedro Pereira <pedrom314@gmail.com>
2018-01-31 00:33:38 -0200

    Below is show the main changes done in this version: - Implemented the function OnClickListener for "botão jogar"

--- app/src/main/java/adivinha/decisionmind/com/adivinha/MainActivity.java
+++ app/src/main/java/adivinha/decisionmind/com/adivinha/MainActivity.java
@@ -2,8 +2,11 @@ package adivinha.decisionmind.com.adivinha;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +21,17 @@ public class MainActivity extends AppCompatActivity {
         botaoJogar = findViewById(R.id.botaoJogarId);
         textoResultado = findViewById(R.id.resultadoId);
 
-        textoResultado.setText("Texto alterado");
+        //textoResultado.setText("Texto alterado");
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Random randomico = new Random();
+                int numeroAleatorio = randomico.nextInt(10);
+                
+                textoResultado.setText(numeroAleatorio);
+            }
+        });
     }
 }
